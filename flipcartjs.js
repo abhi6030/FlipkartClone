2f8ab07ebb68866ae7a6d52f7d00aa3bbb1ee556
abhi6030 <85979880+abhi6030@users.noreply.github.com>
2021-11-20 16:12:40 +0530

    Add files via upload

---
+++ flipcartjs.js
@@ -0,0 +1,15 @@
+document. getElementById("loginid"). onclick = function () {
+    location. href = "file:///D:/FlipkartClone/loginhtml.html";
+    };
+
+
+    document. getElementById("logincart"). onclick = function () {
+        location. href = "file:///D:/FlipkartClone/carthtml.html";
+        };
+        
+
+        document. getElementById("logincartbtn"). onclick = function () {
+            location. href = "file:///D:/FlipkartClone/carthtml.html";
+            };
+
+           
---
+++ loginjs.js
@@ -0,0 +1,3 @@
+document. getElementById("back"). onclick = function () {
+    location. href = "file:///D:/FlipkartClone/flipkarthtml.html";
+    };
